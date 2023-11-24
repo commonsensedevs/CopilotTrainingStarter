b082f3e3234e19b494525f0515b7e976f89074e4
Gomti Mehta <gomtiprofessional@gmail.com>
2023-11-24 14:09:22 -0600

    CheckPoint1 - Strong typed product

---
+++ src/app/products/Iproduct.ts
@@ -0,0 +1,6 @@
+//Create IProduct iterface with ProductName, ProductCode and ProductPrice properties
+export interface IProduct {
+    ProductName: string;
+    ProductCode: string;
+    ProductPrice: number;
+}
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './Iproduct';
 
 @Component({
     selector: 'app-product-list',
@@ -7,29 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
  
-    //Create a products array with ProductName, ProductCode and ProductPrice properties
-    products = [
+    products: IProduct[] = [
         {
-            ProductName: 'Product 1',
-            ProductCode: 'P1',
-            ProductPrice: 100
+          ProductName: 'Product 1',
+          ProductCode: 'P1',
+          ProductPrice: 100
         },
         {
-            ProductName: 'Product 2',
-            ProductCode: 'P2',
-            ProductPrice: 200
+          ProductName: 'Product 2',
+          ProductCode: 'P2',
+          ProductPrice: 200
         },
         {
-            ProductName: 'Product 3',
-            ProductCode: 'P3',
-            ProductPrice: 300
-        },
-        {
-            ProductName: 'Product 4',
-            ProductCode: 'P4',
-            ProductPrice: 400
+          ProductName: 'Product 3',
+          ProductCode: 'P3',
+          ProductPrice: 300
         }
-    ];
+      ];
     constructor() { }
 
     ngOnInit(): void {
